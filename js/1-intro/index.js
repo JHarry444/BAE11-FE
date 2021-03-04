cfbd861614e97f74e0bf78a6037e7a1073047755
jharry444 <jordan.harrison@qa.com>
2021-03-04 16:25:28 +0000

    added js

---
+++ js/1-intro/index.js
@@ -0,0 +1,34 @@
+'use strict'; //stops dodginess
+
+//x = 27; dodgey
+
+let a = 12;
+
+console.log(typeof a, a);
+
+a = "bloop";
+
+
+console.log(typeof a, a);
+
+a = true;
+
+
+console.log(typeof a, a);
+
+a= 24253453535535n;
+
+
+console.log(typeof a, a);
+
+a= undefined;
+
+
+console.log(typeof a, a);
+
+a = {
+    message: "hello!"
+}
+
+
+console.log(typeof a, a);
