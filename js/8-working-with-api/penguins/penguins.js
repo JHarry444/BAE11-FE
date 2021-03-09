ec33ad8958d7d2c056c7243fe0282beef7ee623b
jharry444 <jordan.harrison@qa.com>
2021-03-09 13:01:24 +0000

    added delete example

--- js/8-working-with-api/penguins/penguins.js
+++ js/8-working-with-api/penguins/penguins.js
@@ -3,16 +3,18 @@
 const output = document.getElementById("output");
 
 function getPenguins() {
-    axios.get("http://localhost:8080/getPenguins")
-        .then(res => {
-            const penguins = res.data;
+  axios.get("http://localhost:8080/getPenguins")
+    .then(res => {
+      output.innerHTML = "";
 
-            penguins.forEach(penguin => {
-                const newPenguin = renderPenguin(penguin);
-                console.log("New penguin: ", newPenguin);
-                output.appendChild(newPenguin);
-            });
-        }).catch(err => console.error(err))
+      const penguins = res.data;
+
+      penguins.forEach(penguin => {
+        const newPenguin = renderPenguin(penguin);
+        console.log("New penguin: ", newPenguin);
+        output.appendChild(newPenguin);
+      });
+    }).catch(err => console.error(err))
 }
 
 {/* <div class="card" style="width: 18rem;">
@@ -24,30 +26,48 @@ function getPenguins() {
 
 function renderPenguin(penguin) {
 
-    const newColumn = document.createElement("div");
-    newColumn.className = "col";
+  const newColumn = document.createElement("div");
+  newColumn.className = "col";
 
-    const newPenguin = document.createElement("div");
-    newPenguin.className = "card";
-    newColumn.appendChild(newPenguin);
+  const newPenguin = document.createElement("div");
+  newPenguin.className = "card";
+  newColumn.appendChild(newPenguin);
 
-    const penguinBody = document.createElement("div");
-    penguinBody.className = "card-body";
-    newPenguin.appendChild(penguinBody);
+  const penguinBody = document.createElement("div");
+  penguinBody.className = "card-body";
+  newPenguin.appendChild(penguinBody);
 
-    const penguinName = document.createElement("h5");
-    penguinName.className = "card-title";
-    penguinName.innerText = penguin.name;
-    penguinBody.appendChild(penguinName);
+  const penguinName = document.createElement("h5");
+  penguinName.className = "card-title";
+  penguinName.innerText = penguin.name;
+  penguinBody.appendChild(penguinName);
 
-    const penguinText = document.createElement("p");
-    penguinText.className = "card-text";
-    penguinText.innerHTML = "Age: " + penguin.age;
-    penguinText.innerHTML += "<br>";
-    penguinText.innerHTML += "Tuxedo Size: " + penguin.tuxedoSize;
-    penguinBody.appendChild(penguinText);
+  const penguinText = document.createElement("p");
+  penguinText.className = "card-text";
+  penguinText.innerHTML = "Age: " + penguin.age;
+  penguinText.innerHTML += "<br>";
+  penguinText.innerHTML += "Tuxedo Size: " + penguin.tuxedoSize;
+  penguinBody.appendChild(penguinText);
 
-    return newColumn;
+  const penguinFooter = document.createElement("div");
+  penguinFooter.className = "card-footer"
+  newPenguin.appendChild(penguinFooter);
+
+  const deletePenguinButton = document.createElement("a");
+  deletePenguinButton.className = "card-link";
+  deletePenguinButton.innerText = "Delete";
+  deletePenguinButton.addEventListener('click', function () {
+    deletePenguin(penguin.id);
+  });
+  penguinFooter.appendChild(deletePenguinButton);
+
+  return newColumn;
+}
+
+function deletePenguin(id) {
+  axios.delete("http://localhost:8080/removePenguin/" + id)
+    .then(() => getPenguins())
+    .catch(err => console.error(err));
 }
 
 getPenguins();
